11e5bfe62cd3274ad224e28d8fdef7b3b88696fd
Farhana-Jarin <jarinakther1303@gmail.com>
2025-08-10 11:53:06 +0600

    Add files via upload

---
+++ protfolio/app.js
@@ -0,0 +1,17 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleBtn.textContent = document.body.classList.contains("dark")
+    ? "☀️"
+    : "🌙";
+});
